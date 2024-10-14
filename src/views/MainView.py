004e9de61126fb6247a1379cf9ccb3f7052d764c
Sara Wysk <vt007800>
2024-10-14 14:43:16 +0200

    Add requirements.txt file

--- src/views/MainView.py
+++
@@ -1,6 +0,0 @@
-from tkinter import ttk
-
-
-class MainView(ttk.Frame):
-    def __init__(self, container, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
--- src/views/View.py
+++ src/views/View.py
@@ -1,28 +1,63 @@
-from typing import TypedDict
-
-from views.Root import Root
-
-from views.MainView import MainView
+from tkinter import ttk, StringVar, Entry
 
 
-class Frames(TypedDict):
-    main_view: MainView
+class Vie(ttk.Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
 
-class View:
-    def __init__(self):
-        self.root = Root()
-        self.menubar = None
-        self.frames: Frames = {}
+    def _initialize_value_vars(self):
+        self.scan_value = StringVar()
+        self.animal_value = StringVar()
+        self.experimenter_value = StringVar()
 
-        self._add_frame(MainView, "main_view")
+    def _initialize_frames(self):
+        self.entries_frame = ttk.Frame(self)
+        self.file_frame = ttk.Frame(self)
+        self.control_frame = ttk.Frame(self)
 
-    def _add_frame(self, frame, name: str):
-        self.frames[name] = frame(self.root)
-        self.frames[name].grid(row=0, column=0, sticky="nsew")
+    def _initialize_widgets(self):
+        self.scan_label = ttk.Label(
+            self.entries_frame,
+            text="Scan:",
+        )
+        self.scan_entry = Entry(
+            self.entries_frame,
+            textvariable=self.scan_value,
+            width=15,
+            validate="key",
+        )
 
-    def switch(self, name: str):
-        frame = self.frames[name]
-        frame.tkraise()
+        self.animal_label = ttk.Label(
+            self.entries_frame,
+            text="Animal:",
+        )
+        self.animal_entry = Entry(
+            self.entries_frame,
+            textvariable=self.animal_value,
+            width=15,
+            validate="key",
+        )
 
-    def start_mainloop(self):
-        self.root.mainloop()
+        self.experimenter_label = ttk.Label(
+            self.entries_frame,
+            text="Experimenter:",
+        )
+        self.experimenter_entry = Entry(
+            self.entries_frame,
+            textvariable=self.experimenter_value,
+            width=15,
+            validate="key",
+        )
+
+    def _display_frames(self):
+        self.entries_frame.grid(row=0, column=0, sticky="we")
+        self.file_frame.grid(row=1, column=0, sticky="we")
+        self.control_frame.grid(row=1, column=1, sticky="e")
+
+    def _display_widgets(self):
+        self.scan_label.grid(row=0, column=0, sticky="w")
+        self.scan_entry.grid(row=0, column=1, sticky="w")
+        self.animal_label.grid(row=0, column=2, sticky="w")
+        self.animal_entry.grid(row=0, column=3, sticky="w")
+        self.experimenter_label.grid(row=0, column=4, sticky="w")
+        self.experimenter_entry.grid(row=0, column=5, sticky="w")
