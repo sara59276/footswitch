256c4a022664aec3d68ff4e32c729879ad2ad6b8
Sara Wysk <vt007800>
2024-10-22 10:01:01 +0200

    Customize window icon

--- src/models/ImageManager.py
+++ src/models/ImageManager.py
@@ -7,16 +7,14 @@ from PIL.ImageTk import PhotoImage
 class ImageManager:
 
     @staticmethod
-    def get_image(filename: str, width: int = 30, height: int = 30) -> PhotoImage:
+    def get_filepath(filename: str) -> Path:
         base_dir = Path(__file__).resolve().parent
         src_dir = base_dir.parent
-        icon_path = src_dir / 'resources' / 'images' / filename
+        return src_dir / 'resources' / 'images' / filename
+
+    @staticmethod
+    def get_image(filename: str, width: int = 30, height: int = 30) -> PhotoImage:
+        icon_path = ImageManager.get_filepath(filename)
         icon = Image.open(icon_path)
         resized_icon = icon.resize((width, height))
         return ImageTk.PhotoImage(resized_icon)
-
-    @staticmethod
-    def get_app_icon():
-        base_dir = Path(__file__).resolve().parent
-        src_dir = base_dir.parent
-        return src_dir / 'resources' / 'images' / 'app_icon.ico'
--- src/views/Root.py
+++ src/views/Root.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import *
 
 from constants.ui_config import WINDOW_START_WIDTH, WINDOW_START_HEIGHT, WINDOW_MIN_WIDTH, WINDOW_MIN_HEIGHT, APP_TITLE
 from models.ImageManager import ImageManager
@@ -11,7 +11,8 @@ class Root(Tk):
         self.geometry(f"{WINDOW_START_WIDTH}x{WINDOW_START_HEIGHT}")
         self.minsize(width=WINDOW_MIN_WIDTH, height=WINDOW_MIN_HEIGHT)
         self.title(APP_TITLE)
-        # self.iconbitmap(ImageManager.get_app_icon()) # TODO fix
+        window_icon = PhotoImage(file=ImageManager.get_filepath("app_icon.png"))
+        self.iconphoto(False, window_icon)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
